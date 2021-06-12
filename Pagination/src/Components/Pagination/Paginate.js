972ed638fcdf001e4a2618147ca5a3068167885a
Saurabh Singh <68112187+shivanshthakur4u@users.noreply.github.com>
2021-06-12 12:47:00 +0530

    Add files via upload

---
+++ Pagination/src/Components/Pagination/Paginate.js
@@ -0,0 +1,10 @@
+import _ from 'lodash';
+
+export default  function Paginate(items, pageNumber, pageSize){
+   const startIndex = (pageNumber - 1) * pageSize;
+
+   return _(items)
+            .slice(startIndex)
+            .take(pageSize)
+            .value();
+}
